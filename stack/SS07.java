fcbabf7bddde092e96f90393d8f7611d6f30ac00
annpurna-jha <annpurnajha4@gmail.com>
2024-12-16 21:56:29 +0530

    stock-span-problem

---
+++ stack/SS07.java
@@ -0,0 +1,32 @@
+package Stack;
+import java.util.Stack;
+public class SS07 {
+
+    public static void stockSpan(int[] stock, int[] span){
+        Stack<Integer> st = new Stack<>();
+        span[0] = 1;// for 1st pos it will always be 1
+        st.push(0);
+        for(int i=1;i<stock.length;i++){
+            int currPrice = stock[i];
+            
+            while (!st.isEmpty() && currPrice >= stock[st.peek()]) {
+                st.pop();
+            }
+            if(st.isEmpty()) span[i] = i+1;
+            else{
+                int prevHigh = st.peek();
+                span[i] = i - prevHigh;
+            }
+            st.push(i);
+        }
+    }
+    public static void main(String[] args) {
+        int[] stock = {100,80,60,70,60,85,100};
+        int[] span = new int[stock.length];
+        stockSpan(stock,span);
+
+        for(int i=0;i<span.length;i++){
+            System.out.print(span[i]+" ");
+        }
+    }
+}
